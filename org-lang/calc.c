9c78ced74224e864eb012b4b3dd183f49c8779b1
stnamco <lbnamco15@gmail.com>
2020-11-08 09:40:50 +0900

    enbale to use unary operator

--- org-lang/calc.c
+++ org-lang/calc.c
@@ -149,6 +149,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -164,6 +165,27 @@ Node *expr() {
   }
 }
 
+Node *mul() {
+   Node *node = unary();
+
+  for (;;) {
+    if (consume('*'))
+        node = new_node(ND_MUL, node, unary());
+    else if (consume('/'))
+        node = new_node(ND_DIV, node, unary());
+    else
+      return node;
+  }
+}
+
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), unary());
+  return primary();
+}
+
 Node *primary() {
   // 次のトークンが"("なら、"(" expr ")"のはず
   if (consume('(')) {
@@ -175,18 +197,6 @@ Node *primary() {
   return new_node_num(expect_number());
 }
 
-Node *mul() {
-  Node *node = primary();
-
-  for (;;) {
-    if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
-    else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
-    else
-      return node;
-  }
-}
 
 void gen(Node *node) {
   if (node->kind == ND_NUM) {
