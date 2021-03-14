b0be87fc0d8dc73c52560472930d3d448579503d
stnamco <lbnamco15@gmail.com>
2021-03-14 09:52:48 +0900

    enable to build

--- org-lang/codegen.c
+++ org-lang/codegen.c
@@ -1,97 +1,232 @@
 #include "calc.h"
 
+static int depth;
+
+static void push(void) {
+  printf("  push %%rax\n");
+  depth++;
+}
+
+static void pop(char *arg) {
+  printf("  pop %s\n", arg);
+  depth--;
+}
+
+// Compute the absolute address of a given node.
+// It's an error if a given node does not reside in memory.
+static int align_to(int n, int align) {
+  return (n + align - 1) / align * align;
+}
+
 // Pushes the given node's address to the stack.
 void gen_addr(Node *node) {
-  if (node->kind == ND_LVAR) {
-    int offset = (node->name - 'a' + 1) * 8;
-    printf("  lea rax, [rbp-%d]\n", offset);
-    printf("  push rax\n");
+  if (node->kind == ND_VAR) {
+    printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
     return;
   }
+  // if (node->kind == ND_LVAR) {
+  //   int offset = (node->name - 'a' + 1) * 8;
+  //   printf("  lea rax, [rbp-%d]\n", offset);
+  //   printf("  push rax\n");
+  //   return;
+  // }
 
   error("not an lvalue");
 }
 
-void load() {
-  printf("  pop rax\n");
-  printf("  mov rax, [rax]\n");
-  printf("  push rax\n");
-}
+// void load() {
+//   printf("  pop rax\n");
+//   printf("  mov rax, [rax]\n");
+//   printf("  push rax\n");
+// }
 
-void store() {
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-  printf("  mov [rax], rdi\n");
-  printf("  push rdi\n");
-}
+// void store() {
+//   printf("  pop rdi\n");
+//   printf("  pop rax\n");
+//   printf("  mov [rax], rdi\n");
+//   printf("  push rdi\n");
+// }
 
 // Generate code for a given node.
-void gen(Node *node) {
+static void gen_expr(Node *node) {
   switch (node->kind) {
     case ND_NUM:
-      printf("  push %d\n", node->val);
+      printf("  mov $%d, %%rax\n", node->val);
       return;
-    case ND_EXPR_STMT:
-      gen(node->lhs);
-      printf("  add rsp, 8\n");
+    case ND_NEG:
+      gen_expr(node->lhs);
+      printf("  neg %%rax\n");
       return;
-    case ND_LVAR:
+    case ND_VAR:
       gen_addr(node);
-      load();
+      printf("  mov (%%rax), %%rax\n");
       return;
     case ND_ASSIGN:
       gen_addr(node->lhs);
-      gen(node->rhs);
-      store();
-      return;
-    case ND_RETURN:
-      gen(node->lhs);
-      printf("  pop rax\n");
-      printf("  jmp .Lreturn\n");
+      push();
+      gen_expr(node->rhs);
+      pop("%rdi");
+      printf("  mov %%rax, (%%rdi)\n");
       return;
   }
 
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
+  gen_expr(node->rhs);
+  push();
+  gen_expr(node->lhs);
+  pop("%rdi");
 
   switch (node->kind) {
     case ND_ADD:
-      printf("  add rax, rdi\n");
-      break;
+      printf("  add %%rdi, %%rax\n");
+      return;
     case ND_SUB:
-      printf("  sub rax, rdi\n");
-      break;
+      printf("  sub %%rdi, %%rax\n");
+      return;
     case ND_MUL:
-      printf("  imul rax, rdi\n");
-      break;
+      printf("  imul %%rdi, %%rax\n");
+      return;
     case ND_DIV:
       printf("  cqo\n");
-      printf("  idiv rdi\n");
-      break;
+      printf("  idiv %%rdi\n");
+      return;
     case ND_EQ:
-      printf("  cmp rax, rdi\n");
-      printf("  sete al\n");
-      printf("  movzb rax, al\n");
-      break;
     case ND_NE:
-      printf("  cmp rax, rdi\n");
-      printf("  setne al\n");
-      printf("  movzb rax, al\n");
-      break;
     case ND_LT:
-      printf("  cmp rax, rdi\n");
-      printf("  setl al\n");
-      printf("  movzb rax, al\n");
-      break;
     case ND_LE:
-      printf("  cmp rax, rdi\n");
-      printf("  setle al\n");
-      printf("  movzb rax, al\n");
-      break;
+      printf("  cmp %%rdi, %%rax\n");
+
+      if (node->kind == ND_EQ)
+        printf("  sete %%al\n");
+      else if (node->kind == ND_NE)
+        printf("  setne %%al\n");
+      else if (node->kind == ND_LT)
+        printf("  setl %%al\n");
+      else if (node->kind == ND_LE)
+        printf("  setle %%al\n");
+
+      printf("  movzb %%al, %%rax\n");
+      return;
   }
 
-  printf("  push rax\n");
+  error("invalid expression");
 }
+
+static void gen_stmt(Node *node) {
+  // if (node->kind == ND_EXPR_STMT) {
+  //   gen_expr(node->lhs);
+  //   return;
+  // }
+  switch (node->kind) {
+    case ND_RETURN:
+      gen_expr(node->lhs);
+      printf("  jmp .L.return\n");
+      return;
+    case ND_EXPR_STMT:
+      gen_expr(node->lhs);
+      return;
+  }
+
+  error("invalid statement");
+}
+
+// Assign offsets to local variables.
+static void assign_lvar_offsets(Function *prog) {
+  int offset = 0;
+  for (Obj *var = prog->locals; var; var = var->next) {
+    offset += 8;
+    var->offset = -offset;
+  }
+  prog->stack_size = align_to(offset, 16);
+}
+
+void codegen(Function *prog) {
+  assign_lvar_offsets(prog);
+
+  printf("  .globl main\n");
+  printf("main:\n");
+
+  // Prologue
+  printf("  push %%rbp\n");
+  printf("  mov %%rsp, %%rbp\n");
+  printf("  sub $%d, %%rsp\n", prog->stack_size);
+
+  for (Node *n = prog->body; n; n = n->next) {
+    gen_stmt(n);
+    assert(depth == 0);
+  }
+
+  printf("  mov %%rbp, %%rsp\n");
+  printf("  pop %%rbp\n");
+  printf("  ret\n");
+}
+
+// // Generate code for a given node.
+// void gen(Node *node) {
+//   switch (node->kind) {
+//     case ND_NUM:
+//       printf("  push %d\n", node->val);
+//       return;
+//     case ND_EXPR_STMT:
+//       gen(node->lhs);
+//       printf("  add rsp, 8\n");
+//       return;
+//     case ND_LVAR:
+//       gen_addr(node);
+//       load();
+//       return;
+//     case ND_ASSIGN:
+//       gen_addr(node->lhs);
+//       gen(node->rhs);
+//       store();
+//       return;
+//     case ND_RETURN:
+//       gen(node->lhs);
+//       printf("  pop rax\n");
+//       printf("  jmp .Lreturn\n");
+//       return;
+//   }
+
+//   gen(node->lhs);
+//   gen(node->rhs);
+
+//   printf("  pop rdi\n");
+//   printf("  pop rax\n");
+
+//   switch (node->kind) {
+//     case ND_ADD:
+//       printf("  add rax, rdi\n");
+//       break;
+//     case ND_SUB:
+//       printf("  sub rax, rdi\n");
+//       break;
+//     case ND_MUL:
+//       printf("  imul rax, rdi\n");
+//       break;
+//     case ND_DIV:
+//       printf("  cqo\n");
+//       printf("  idiv rdi\n");
+//       break;
+//     case ND_EQ:
+//       printf("  cmp rax, rdi\n");
+//       printf("  sete al\n");
+//       printf("  movzb rax, al\n");
+//       break;
+//     case ND_NE:
+//       printf("  cmp rax, rdi\n");
+//       printf("  setne al\n");
+//       printf("  movzb rax, al\n");
+//       break;
+//     case ND_LT:
+//       printf("  cmp rax, rdi\n");
+//       printf("  setl al\n");
+//       printf("  movzb rax, al\n");
+//       break;
+//     case ND_LE:
+//       printf("  cmp rax, rdi\n");
+//       printf("  setle al\n");
+//       printf("  movzb rax, al\n");
+//       break;
+//   }
+
+//   printf("  push rax\n");
+// }
--- org-lang/parse.c
+++ org-lang/parse.c
@@ -1,45 +1,88 @@
 #include "calc.h"
 
-Node *new_node(NodeKind kind) {
+// All local variable instances created during parsing are
+// accumulated to this list.
+Obj *locals;
+
+static Node *expr(Token **rest, Token *tok);
+static Node *expr_stmt(Token **rest, Token *tok);
+static Node *assign(Token **rest, Token *tok);
+static Node *equality(Token **rest, Token *tok);
+static Node *relational(Token **rest, Token *tok);
+static Node *add(Token **rest, Token *tok);
+static Node *mul(Token **rest, Token *tok);
+static Node *unary(Token **rest, Token *tok);
+static Node *primary(Token **rest, Token *tok);
+
+// Node *new_node(NodeKind kind) {
+//   Node *node = calloc(1, sizeof(Node));
+//   node->kind = kind;
+//   return node;
+// }
+
+// Find a local variable by name.
+static Obj *find_var(Token *tok) {
+  for (Obj *var = locals; var; var = var->next)
+    if (strlen(var->name) == tok->len &&
+        !strncmp(tok->loc, var->name, tok->len))
+      return var;
+  return NULL;
+}
+
+static Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = kind;
   return node;
 }
 
-Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
   Node *node = new_node(kind);
   node->lhs = lhs;
   node->rhs = rhs;
   return node;
 }
 
-Node *new_unary(NodeKind kind, Node *expr) {
+static Node *new_unary(NodeKind kind, Node *expr) {
   Node *node = new_node(kind);
   node->lhs = expr;
   return node;
 }
 
-Node *new_num(int val) {
+static Node *new_num(int val) {
   Node *node = new_node(ND_NUM);
   node->val = val;
   return node;
 }
 
-Node *new_lvar(char name) {
-  Node *node = new_node(ND_LVAR);
-  node->name = name;
+// Node *new_lvar(char name) {
+//   Node *node = new_node(ND_LVAR);
+//   node->name = name;
+//   return node;
+// }
+
+static Node *new_var_node(Obj *var) {
+  Node *node = new_node(ND_VAR);
+  node->var = var;
   return node;
 }
 
-Node *stmt();
-Node *expr();
-Node *assign();
-Node *equality();
-Node *relational();
-Node *add();
-Node *mul();
-Node *unary();
-Node *primary();
+static Obj *new_lvar(char *name) {
+  Obj *var = calloc(1, sizeof(Obj));
+  var->name = name;
+  var->next = locals;
+  locals = var;
+  return var;
+}
+
+// Node *stmt();
+// Node *expr();
+// Node *assign();
+// Node *equality();
+// Node *relational();
+// Node *add();
+// Node *mul();
+// Node *unary();
+// Node *primary();
 
 // program = stmt*
 Node *program() {
@@ -54,108 +97,276 @@ Node *program() {
   return head.next;
 }
 
+// // stmt = "return" expr ";"
+// //      | expr ";"
+// Node *stmt() {
+//   if (consume("return")) {
+//     Node *node = new_unary(ND_RETURN, expr());
+//     expect(";");
+//     return node;
+//   }
+
+//   Node *node = new_unary(ND_EXPR_STMT, expr());
+//   expect(";");
+//   return node;
+// }
+
 // stmt = "return" expr ";"
-//      | expr ";"
-Node *stmt() {
-  if (consume("return")) {
-    Node *node = new_unary(ND_RETURN, expr());
-    expect(";");
+//      | expr-stmt
+static Node *stmt(Token **rest, Token *tok) {
+  if (equal(tok, "return")) {
+    Node *node = new_unary(ND_RETURN, expr(&tok, tok->next));
+    *rest = skip(tok, ";");
     return node;
   }
 
-  Node *node = new_unary(ND_EXPR_STMT, expr());
-  expect(";");
+  return expr_stmt(rest, tok);
+}
+
+// expr-stmt = expr ";"
+static Node *expr_stmt(Token **rest, Token *tok) {
+  Node *node = new_unary(ND_EXPR_STMT, expr(&tok, tok));
+  *rest = skip(tok, ";");
   return node;
 }
+
+// // expr = assign
+// Node *expr() { return assign(); }
 
 // expr = assign
-Node *expr() { return assign(); }
+static Node *expr(Token **rest, Token *tok) { return assign(rest, tok); }
+
+// // assign = equality ("=" assign)?
+// Node *assign() {
+//   Node *node = equality();
+//   if (consume("=")) node = new_binary(ND_ASSIGN, node, assign());
+//   return node;
+// }
 
 // assign = equality ("=" assign)?
-Node *assign() {
-  Node *node = equality();
-  if (consume("=")) node = new_binary(ND_ASSIGN, node, assign());
+static Node *assign(Token **rest, Token *tok) {
+  Node *node = equality(&tok, tok);
+  if (equal(tok, "="))
+    node = new_binary(ND_ASSIGN, node, assign(&tok, tok->next));
+  *rest = tok;
   return node;
 }
 
+// // equality = relational ("==" relational | "!=" relational)*
+// Node *equality() {
+//   Node *node = relational();
+
+//   for (;;) {
+//     if (consume("=="))
+//       node = new_binary(ND_EQ, node, relational());
+//     else if (consume("!="))
+//       node = new_binary(ND_NE, node, relational());
+//     else
+//       return node;
+//   }
+// }
+
 // equality = relational ("==" relational | "!=" relational)*
-Node *equality() {
-  Node *node = relational();
+static Node *equality(Token **rest, Token *tok) {
+  Node *node = relational(&tok, tok);
 
   for (;;) {
-    if (consume("=="))
-      node = new_binary(ND_EQ, node, relational());
-    else if (consume("!="))
-      node = new_binary(ND_NE, node, relational());
-    else
-      return node;
+    if (equal(tok, "==")) {
+      node = new_binary(ND_EQ, node, relational(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "!=")) {
+      node = new_binary(ND_NE, node, relational(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
+
+// // relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+// Node *relational() {
+//   Node *node = add();
+
+//   for (;;) {
+//     if (consume("<"))
+//       node = new_binary(ND_LT, node, add());
+//     else if (consume("<="))
+//       node = new_binary(ND_LE, node, add());
+//     else if (consume(">"))
+//       node = new_binary(ND_LT, add(), node);
+//     else if (consume(">="))
+//       node = new_binary(ND_LE, add(), node);
+//     else
+//       return node;
+//   }
+// }
 
 // relational = add ("<" add | "<=" add | ">" add | ">=" add)*
-Node *relational() {
-  Node *node = add();
+static Node *relational(Token **rest, Token *tok) {
+  Node *node = add(&tok, tok);
 
   for (;;) {
-    if (consume("<"))
-      node = new_binary(ND_LT, node, add());
-    else if (consume("<="))
-      node = new_binary(ND_LE, node, add());
-    else if (consume(">"))
-      node = new_binary(ND_LT, add(), node);
-    else if (consume(">="))
-      node = new_binary(ND_LE, add(), node);
-    else
-      return node;
+    if (equal(tok, "<")) {
+      node = new_binary(ND_LT, node, add(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "<=")) {
+      node = new_binary(ND_LE, node, add(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, ">")) {
+      node = new_binary(ND_LT, add(&tok, tok->next), node);
+      continue;
+    }
+
+    if (equal(tok, ">=")) {
+      node = new_binary(ND_LE, add(&tok, tok->next), node);
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
+
+// // add = mul ("+" mul | "-" mul)*
+// Node *add() {
+//   Node *node = mul();
+
+//   for (;;) {
+//     if (consume("+"))
+//       node = new_binary(ND_ADD, node, mul());
+//     else if (consume("-"))
+//       node = new_binary(ND_SUB, node, mul());
+//     else
+//       return node;
+//   }
+// }
 
 // add = mul ("+" mul | "-" mul)*
-Node *add() {
-  Node *node = mul();
+static Node *add(Token **rest, Token *tok) {
+  Node *node = mul(&tok, tok);
 
   for (;;) {
-    if (consume("+"))
-      node = new_binary(ND_ADD, node, mul());
-    else if (consume("-"))
-      node = new_binary(ND_SUB, node, mul());
-    else
-      return node;
+    if (equal(tok, "+")) {
+      node = new_binary(ND_ADD, node, mul(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "-")) {
+      node = new_binary(ND_SUB, node, mul(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
+
+// // mul = unary ("*" unary | "/" unary)*
+// Node *mul() {
+//   Node *node = unary();
+
+//   for (;;) {
+//     if (consume("*"))
+//       node = new_binary(ND_MUL, node, unary());
+//     else if (consume("/"))
+//       node = new_binary(ND_DIV, node, unary());
+//     else
+//       return node;
+//   }
+// }
 
 // mul = unary ("*" unary | "/" unary)*
-Node *mul() {
-  Node *node = unary();
+static Node *mul(Token **rest, Token *tok) {
+  Node *node = unary(&tok, tok);
 
   for (;;) {
-    if (consume("*"))
-      node = new_binary(ND_MUL, node, unary());
-    else if (consume("/"))
-      node = new_binary(ND_DIV, node, unary());
-    else
-      return node;
+    if (equal(tok, "*")) {
+      node = new_binary(ND_MUL, node, unary(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "/")) {
+      node = new_binary(ND_DIV, node, unary(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
 
-// unary = ("+" | "-")? unary
+// // unary = ("+" | "-")? unary
+// //       | primary
+// Node *unary() {
+//   if (consume("+")) return unary();
+//   if (consume("-")) return new_binary(ND_SUB, new_num(0), unary());
+//   return primary();
+// }
+
+// unary = ("+" | "-") unary
 //       | primary
-Node *unary() {
-  if (consume("+")) return unary();
-  if (consume("-")) return new_binary(ND_SUB, new_num(0), unary());
-  return primary();
+static Node *unary(Token **rest, Token *tok) {
+  if (equal(tok, "+")) return unary(rest, tok->next);
+
+  if (equal(tok, "-")) return new_unary(ND_NEG, unary(rest, tok->next));
+
+  return primary(rest, tok);
 }
 
+// // primary = "(" expr ")" | ident | num
+// Node *primary() {
+//   if (consume("(")) {
+//     Node *node = expr();
+//     expect(")");
+//     return node;
+//   }
+
+//   Token *tok = consume_ident();
+//   if (tok) return new_lvar(*tok->str);
+
+//   return new_num(expect_number());
+// }
+
 // primary = "(" expr ")" | ident | num
-Node *primary() {
-  if (consume("(")) {
-    Node *node = expr();
-    expect(")");
+static Node *primary(Token **rest, Token *tok) {
+  if (equal(tok, "(")) {
+    Node *node = expr(&tok, tok->next);
+    *rest = skip(tok, ")");
     return node;
   }
 
-  Token *tok = consume_ident();
-  if (tok) return new_lvar(*tok->str);
+  if (tok->kind == TK_IDENT) {
+    Obj *var = find_var(tok);
+    if (!var) var = new_lvar(strndup(tok->loc, tok->len));
+    *rest = tok->next;
+    return new_var_node(var);
+  }
 
-  return new_num(expect_number());
+  if (tok->kind == TK_NUM) {
+    Node *node = new_num(tok->val);
+    *rest = tok->next;
+    return node;
+  }
+
+  error_tok(tok, "expected an expression");
+}
+
+// program = stmt*
+Function *parse(Token *tok) {
+  Node head = {};
+  Node *cur = &head;
+
+  while (tok->kind != TK_EOF) cur = cur->next = stmt(&tok, tok);
+
+  Function *prog = calloc(1, sizeof(Function));
+  prog->body = head.next;
+  prog->locals = locals;
+  return prog;
 }
